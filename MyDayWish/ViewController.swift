8b553cda82c6a2352bebe2283e20b3bb44e4f09f
Sergei <dhusara@icloud.com>
2021-01-24 17:42:57 +0200

    backgroundColor

--- MyDayWish/ViewController.swift
+++ MyDayWish/ViewController.swift
@@ -14,7 +14,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
+    
+    @IBOutlet weak var wishLabel: UILabel!
+    
+}
 
 
+// MARK: - SwiftUI
+import SwiftUI
+
+struct MainVCProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContainerView().edgesIgnoringSafeArea(.all)
+        }
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let mainVC = ViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<MainVCProvider.ContainerView>) -> ViewController {
+            return mainVC
+        }
+        
+        func updateUIViewController(_ uiViewController: MainVCProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<MainVCProvider.ContainerView>) {
+            
+        }
+    }
 }
 
